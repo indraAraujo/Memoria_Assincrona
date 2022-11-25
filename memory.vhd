c12ca8177facd9c9e7074597484d3fc2d8d2a03e
Indra Araujo <indra.araujo.santos@gmail.com>
2022-11-24 21:49:44 -0300

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -5,14 +5,35 @@ use ieee.std_logic_arith.all;
 
 entity memory is
     port(
-        ler     : in std_logic;
-        endereco    : in std_logic_vector(7 downto 0);
+        ler         : in std_logic;
+        endereco    : in std_logic_vector(4 downto 0);
         resposta    : out std_logic_vector(7 downto 0);
     );
 end entity;
 
 architecture behavior of memory is
-signal memory_inst: std_logic_vector() %%fazer vetor de vetores
+
+TYPE matriz IS ARRAY (INTEGER RANGE 0 TO 15) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+CONSTANT memory_inst : matriz :=
+("00000001",
+"10010111",
+"00100010",
+"00001100",
+"10011000",
+"01001000",
+"01000000",
+"00011011",
+"00000001",
+"10010111",
+"00100010",
+"00001100",
+"10011000",
+"01001000",
+"01000000",
+"00011011");
+
+
 begin
 
     process(ler)
@@ -23,4 +44,5 @@ begin
                 resposta <= '0';
             }
         end process;
+        
 end behavior;
