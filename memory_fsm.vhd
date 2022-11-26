19600d732d76f1a8ed6e893014cc72d0e5498b02
Indra Araujo <indra.araujo.santos@gmail.com>
2022-11-25 21:39:43 -0300

    Update memory_fsm.vhd

--- memory_fsm.vhd
+++ memory_fsm.vhd
@@ -9,46 +9,44 @@ entity memory_fsm is
         ack_entrada : in std_logic;
         ack_saida   : out std_logic;
         dadoPrt     : out std_logic;
-        ler         : out std_logic;
+        ler         : out std_logic
     );
 end entity;
 
 architecture behavior of memory_fsm is
 
-signal prox_estado  : std_logic_vector(3 downto 0);
-signal estado_atual : std_logic_vector(3 downto 0);
+signal prox_estado  : std_logic_vector(3 downto 0):="0000";
+signal estado_atual : std_logic_vector(3 downto 0):="0000";
 
 begin
 
     process(reqLeit, ack_entrada)
         begin
 
-            if(estado_atual = "0000" && reqLeit='0')then
+            if((estado_atual = "0000") and (reqLeit='0'))then
             
-                elsif(estado_atual = "0000" && reqLeit='1')
-                prox_estado <= "0001"
+                elsif((estado_atual = "0000") and (reqLeit='1')) then
+                prox_estado <= "0001";
             end if;
             
-            if (estado_atual = "0001" && reqLeit='1' && ack_entrada='0')
+            if ((estado_atual = "0001") and (reqLeit='1') and (ack_entrada='0')) then
                 ack_saida <= '1';
                 ler <= '0';
                 prox_estado <="0010";
 
-            elsif (estado_atual = "0010" && reqLeit='0' && ack_entrada='0')
+            elsif ((estado_atual = "0010") and (reqLeit='0') and (ack_entrada='0')) then
                 ack_saida <= '0';
                 ler <= '1';
                 dadoPrt <= '1';
                 prox_estado <="0011";
 
-            elsif (estado_atual = "0011" && reqLeit='0' && ack_entrada='1')
+            elsif ((estado_atual = "0011") and (reqLeit='0') and (ack_entrada='1')) then
                 dadoPrt <= '0';
                 ler <= '0';
                 prox_estado <= "0000";
             
             end if;
-
             estado_atual <= prox_estado;
-
 
         end process;
 end behavior;
