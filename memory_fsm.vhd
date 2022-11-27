a6746f54024f653631591703beec4ca916cba6d1
indraAraujo <indra.araujo.santos@gmail.com>
2022-11-27 20:40:17 -0300

    nova fsm

--- memory_fsm.vhd
+++ memory_fsm.vhd
@@ -2,51 +2,64 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
-
 entity memory_fsm is
-    port(
-        reqLeit     : in std_logic;
-        ack_entrada : in std_logic;
-        ack_saida   : out std_logic;
-        dadoPrt     : out std_logic;
-        ler         : out std_logic
-    );
+	port(
+            reqLeit	    : in std_logic;
+            ack_entrada : in std_logic;
+            dadoPrt     : out std_logic;
+            ler         : out std_logic;
+            ack_saida   : out std_logic
+		);
 end entity;
 
 architecture behavior of memory_fsm is
-
-signal prox_estado  : std_logic_vector(3 downto 0):="0000";
-signal estado_atual : std_logic_vector(3 downto 0):="0000";
-
+type state_name is (zero, first, second, third); -- nome dos estados
+signal state, next_state : state_name;  -- tipo de dado dos estados
 begin
 
-    process(reqLeit, ack_entrada)
-        begin
+-- processo sequencial TERMINAR
+process(reqLeit, ack_entrada)
+begin
+	state <= next_state;
+end process;
 
-            if((estado_atual = "0000") and (reqLeit='0'))then
-            
-                elsif((estado_atual = "0000") and (reqLeit='1')) then
-                prox_estado <= "0001";
-            end if;
-            
-            if ((estado_atual = "0001") and (reqLeit='1') and (ack_entrada='0')) then
-                ack_saida <= '1';
-                ler <= '0';
-                prox_estado <="0010";
+-- processo combinacional da saida da FSM
+process (state)
+begin
+	case state is
+		when zero =>
+			ler <= '0;
+            ack_saida <= '0';
+            dadoPrt <= '0';
+		when first =>
+            ler <= '0;
+            ack_saida <= '1';
+            dadoPrt <= '0';
+		when second =>
+            ler <= '0;
+            ack_saida <= '1';
+            dadoPrt <= '1';
+		when third =>
+            ler <= '0;
+            ack_saida <= '0';
+            dadoPrt <= '0';
+		end case;
+end process;
 
-            elsif ((estado_atual = "0010") and (reqLeit='0') and (ack_entrada='0')) then
-                ack_saida <= '0';
-                ler <= '1';
-                dadoPrt <= '1';
-                prox_estado <="0011";
+-- processo combinacional da saida da FSM
+process (state)
+begin
+	case state is
+		when zero =>
+			next_state <= first;
+		when first =>
+		    next_state <= second;
+		when second =>
+		    next_state <= third;
+		when third =>
+            next_state <= zero;
+		end case;
+end process;
 
-            elsif ((estado_atual = "0011") and (reqLeit='0') and (ack_entrada='1')) then
-                dadoPrt <= '0';
-                ler <= '0';
-                prox_estado <= "0000";
-            
-            end if;
-            estado_atual <= prox_estado;
-
-        end process;
 end behavior;
+	
---
+++ memory_fsm_old.vhd
@@ -0,0 +1,52 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+entity memory_fsm is
+    port(
+        reqLeit     : in std_logic;
+        ack_entrada : in std_logic;
+        ack_saida   : out std_logic;
+        dadoPrt     : out std_logic;
+        ler         : out std_logic
+    );
+end entity;
+
+architecture behavior of memory_fsm is
+
+signal prox_estado  : std_logic_vector(3 downto 0):="0000";
+signal estado_atual : std_logic_vector(3 downto 0):="0000";
+
+begin
+
+    process(reqLeit, ack_entrada)
+        begin
+
+            if((estado_atual = "0000") and (reqLeit='0'))then
+            
+                elsif((estado_atual = "0000") and (reqLeit='1')) then
+                prox_estado <= "0001";
+            end if;
+            
+            if ((estado_atual = "0001") and (reqLeit='1') and (ack_entrada='0')) then
+                ack_saida <= '1';
+                ler <= '0';
+                prox_estado <="0010";
+
+            elsif ((estado_atual = "0010") and (reqLeit='0') and (ack_entrada='0')) then
+                ack_saida <= '0';
+                ler <= '1';
+                dadoPrt <= '1';
+                prox_estado <="0011";
+
+            elsif ((estado_atual = "0011") and (reqLeit='0') and (ack_entrada='1')) then
+                dadoPrt <= '0';
+                ler <= '0';
+                prox_estado <= "0000";
+            
+            end if;
+            estado_atual <= prox_estado;
+
+        end process;
+end behavior;
